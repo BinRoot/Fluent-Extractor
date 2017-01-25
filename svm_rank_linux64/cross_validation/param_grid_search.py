ffce714e4c0fd0dfe99b258b5fffff2ea7f1776c
Nishant Shukla <nxs@ucla.edu>
2017-01-25 12:35:54 -0800

    added action dataset and improved cross validation

--- svm_rank_linux64/cross_validation/param_grid_search.py
+++ svm_rank_linux64/cross_validation/param_grid_search.py
@@ -10,11 +10,11 @@ a grid search for rank-svm parameters that does best on the validation set.
 import random
 import subprocess
 import os
+import numpy as np
 
 NUM_VID = 45
-NUM_TRAIN = 27
-NUM_CV = 9
-NUM_TEST = 9
+NUM_TRAIN = int(0.7 * NUM_VID)
+NUM_CV = NUM_VID - NUM_TRAIN
 random.seed(1234) # Change the seed for a different split
 
 def write_rows(filename, data):
@@ -24,18 +24,37 @@ def write_rows(filename, data):
             print >> file, row,
     file.close()
 
+
 def run_cmd(command):
     subprocess.call(command, shell=True)
 
-def grid_search(train, validate):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    for c in [0, 0.5, 1, 1.5, 2, 2.5, 3]:
-        print 'c = %f' % (c)
-        model_name = 'model_c_%f' % (c)
-        run_cmd('../svm_rank_learn -c %f -t 1 -d 3 -v 3 train.dat ./models/%s' 
-            % (c, model_name))
-        output = run_cmd('../svm_rank_classify validate.dat ./models/%s > ./models/%s.report' 
-            % (model_name, model_name))
+
+def grid_search():
+    for c in np.logspace(-3, 3, 7):
+        for t in [1, 2, 3]:
+            if t == 1:  # polynomial (s a*b+r)^d
+                for d in [2, 3, 4]:
+                    for r in np.linspace(-10, 10, 3):
+                        model_name = 'model_c_{}_t_{}_d_{}_r_{}'.format(c, t, d, r)
+                        run_cmd('../svm_rank_learn -c {} -t {} -d {} -r {} -v 1 train.dat ./models/{}'
+                                .format(c, t, d, r, model_name))
+                        run_cmd('../svm_rank_classify validate.dat ./models/{} > ./models/{}.report'
+                                .format(model_name, model_name))
+            elif t == 2:  # rbf exp(-gamma ||a-b||^2)
+                for g in np.logspace(-3, 3, 7):
+                    model_name = 'model_c_{}_t_{}_g_{}'.format(c, t, g)
+                    run_cmd('../svm_rank_learn -c {} -t {} -g {} -v 1 train.dat ./models/{}'
+                            .format(c, t, d, model_name))
+                    run_cmd('../svm_rank_classify validate.dat ./models/{} > ./models/{}.report'
+                            .format(model_name, model_name))
+            elif t == 3:  # sigmoid tanh(s a*b + r)
+                for r in np.linspace(-10, 10, 3):
+                    for s in np.linspace(-1, 1, 4):
+                        model_name = 'model_c_{}_t_{}_r_{}_s_{}'.format(c, t, r, s)
+                        run_cmd('../svm_rank_learn -c {} -t {} -r {} -s {} -v 1 train.dat ./models/{}'
+                                .format(c, t, r, s, model_name))
+                        run_cmd('../svm_rank_classify validate.dat ./models/{} > ./models/{}.report'
+                                .format(model_name, model_name))
 
 if __name__ == '__main__':
     # Group fluents by video id
@@ -55,26 +74,21 @@ if __name__ == '__main__':
 
     train_indices = indices[0:NUM_TRAIN]
     validate_indices = indices[NUM_TRAIN:NUM_TRAIN+NUM_CV]
-    test_indices = indices[NUM_TRAIN+NUM_CV:]
 
     train_indices.sort()
     validate_indices.sort()
-    test_indices.sort()
 
     train = [data[qid] for qid in train_indices]
     validate = [data[qid] for qid in validate_indices]
-    test = [data[qid] for qid in test_indices]
 
-    assert len(train) == NUM_TRAIN and len(validate) == NUM_CV and len(test) == NUM_TEST
+    assert len(train) == NUM_TRAIN and len(validate) == NUM_CV
 
     write_rows('train.dat', train)
     write_rows('validate.dat', validate)
-    write_rows('test.dat', test)
 
     # Prepare model directory
     if not os.path.exists('./models'):
         os.makedirs('./models')
 
-
     # Grid search of optimal parameters
-    grid_search(train, validate)
+    grid_search()
