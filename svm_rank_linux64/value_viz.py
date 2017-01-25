a5be8b9adbfa4fdc4e7ffda48b57aaba84671546
kfrankc <frankchen1698@gmail.com>
2017-01-25 15:05:54 -0800

    Added 2D plot of contours using test image

--- svm_rank_linux64/value_viz.py
+++ svm_rank_linux64/value_viz.py
@@ -11,9 +11,7 @@ from sklearn.svm import SVR
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
 
-from matplotlib.image import BboxImage
-from matplotlib.transforms import Bbox, TransformedBbox
-from matplotlib._png import read_png
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
@@ -87,16 +85,15 @@ def draw_arrows(ax, arrows, color='k'):
         ax.add_artist(arrow_art)
         prev_arrow = arrow
 
-def plotImages(xData, yData, image, axis):
-    bb = Bbox.from_bounds(xData,yData,1,1)  
-    bb2 = TransformedBbox(bb,ax.transData)
-    bbox_image = BboxImage(bb2,
-                        norm = None,
-                        origin=None,
-                        clip_on=False)
-
-    bbox_image.set_data(image)
-    axis.add_artist(bbox_image)
+def plotImages(xData, yData, image, ax, z):
+    im = OffsetImage(image, zoom=z)
+    artists = []
+    for i in range(len(xData)):
+        ab = AnnotationBbox(im, (xData[i], yData[i]), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.column_stack([xData, yData]))
+    ax.autoscale()
+    return artists
 
 if __name__ == '__main__':
     # svr_rbf = SVR(kernel='rbf', C=1e1, gamma=1e-1, epsilon=1e0, shrinking=True)
@@ -169,13 +166,23 @@ if __name__ == '__main__':
     ax.scatter3D(centroids_x, centroids_y, centroids_z, color='black', marker='o', s=100)
     for i in range(len(centroids_x)):
         ax.text(centroids_x[i], centroids_y[i], centroids_z[i], meta_info[i], color='red', zorder=1)
-        bird = plt.imread('/home/kfrankc/Desktop/bird.png')
-        plotImages(centroids_x[i], centroids_y[i], bird, ax)
     # for i in range(len(all_arrows)):
     #     draw_arrows(ax, all_arrows[i])
-    ax.autoscale(False)
     plt.title('MDS')
     plt.axis('tight')
+
+    # draw images on separate graph
+    fig_img = plt.figure()
+    ax_img = plt.gca()
+    # surf = ax_img.plot_surface(X_mesh, Y_mesh, Z_test, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0.1, antialiased=True, alpha=0.68)
+    bird = plt.imread('/home/kfrankc/Desktop/resize_bird.png')
+    plotImages(centroids_x, centroids_y, bird, ax_img, 0.5)
+    surf = ax_img.contourf(X_mesh, Y_mesh, Z_test, facecolors=cm.coolwarm)
+    fig_img.colorbar(surf, shrink=0.5, aspect=5)
+    # fig_img.colorbar(surf, shrink=0.5, aspect=5)
+    plt.title('Value Landscape with Image')
+    plt.axis('tight')
+
 
     fig_polished = plt.figure()
     ax_polished = Axes3D(fig_polished)
