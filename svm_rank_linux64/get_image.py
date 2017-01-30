eb6b6407d3015f8777be2067b509fb31c26ffd40
kfrankc <frankchen1698@gmail.com>
2017-01-30 15:25:40 -0800

    Made path to cloth images relative

--- svm_rank_linux64/get_image.py
+++ svm_rank_linux64/get_image.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 # return the image
 def load_image(img_meta):  # 1:45
 	# TODO: make this a relative path
-	base_path = '/home/kfrankc/Desktop/cloth_folding_lite/'
+	base_path = '../cloth_folding_lite/'
 	img_meta_parts = img_meta.split(':')
 	vid_id, img_id = img_meta_parts[0], img_meta_parts[1]
 	img_path = base_path + vid_id + '/' + 'aligned_rgb_' + '0'.zfill(5-len(img_id)) + img_id + '.png'
